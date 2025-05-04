1360ba11d924cfa81e721a63f1f4828040bcdfea
BenMabroukAya <aya.benmabrouk@isimg.tn>
2025-05-04 14:18:26 +0100

    updating user.js & app.js & environment variables

--- routes/user.route.js
+++ routes/user.route.js
@@ -6,7 +6,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 
 /*
